5b93ef2a96cb59aac0cbd4a1f7c04c24a08c6414
PeterEastham <petereastham10@gmail.com>
2021-09-24 06:51:47 -0600

    Main Update
    Update to main.py to have it be a starting point.

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from controller import *
-from service.version_handling import class_csv_headers as cch
-
-Econ = EC.start_controller()
-Pcon = PC.start_controller()
-employees = Econ._EmpRepo.get_all_employees()
+from view.login_page import LoginPage
 
 
-with open("payroll.txt", 'w') as payroll:
-    pay = []
-    for emp in employees:
-        pay_str = Pcon.get_pay_str(emp.id)
-        print(pay_str, end='')
-        pay.append(pay_str)
-
-    payroll.writelines(pay)
+if __name__ == "__main__":
+    LoginPage.main()
