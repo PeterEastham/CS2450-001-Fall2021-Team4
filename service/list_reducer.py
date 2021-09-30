3dfbee35115103e8dd85d139678958fb7011fb61
PeterEastham <petereastham10@gmail.com>
2021-09-30 16:06:06 -0600

    Reducer Inclusion
    Can now reduce dictionaries with strings as keys from a search_string.

--- service/list_reducer.py
+++ service/list_reducer.py
@@ -1,4 +1,14 @@
+import re
 
+def sub_dict_from_keys(dictionary, search_string):
+    subset = _remaker(search_string)
+    return {k:v for (k, v) in dictionary.items() if subset.match(k)}
 
-def sub_dict(dictionary, subset):
-    return {for subset in dictionary}
+def sub_dict_from_values(dictionary, search_string):
+    subset = _remaker(search_string)
+    return {k:v for (k, v) in dictionary.items() if subset.match(v)}
+
+def _remaker(search_string):
+    re_string = "+".join(search_string)
+    re_string += "+"
+    return re.compile(re_string)
