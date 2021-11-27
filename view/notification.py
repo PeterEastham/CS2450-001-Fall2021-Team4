53d6dc075fac526c9ddd00fb51386e6b4f275ae7
PeterEastham <petereastham10@gmail.com>
2021-11-26 18:45:32 -0700

    Notification Window

---
+++ view/notification.py
@@ -0,0 +1,24 @@
+"""
+Using this as a decorator to certain functions.
+Creates a "pop-up" window that disables the underlying window.
+Useful for Permission Handling.
+"""
+import tkinter as tk
+from tkinter import ttk
+
+
+class Notification(tk.Tk):
+
+    def __init__(self, message):
+        super().__init__()
+
+        self.geometry("300x100")
+        self.title("Notice")
+        self.resizable(0, 0)
+        self.rowconfigure(0, weight=3)
+        self.columnconfigure(0, weight=2)
+        self.message = ttk.Label(self, text=message)
+        self.message.grid(column=0, row=0, columnspan=2, ipady=30)
+
+        self.button = ttk.Button(self, text="I Understand", command=self.destroy)
+        self.button.grid(column=1, row=1, padx=10, pady=10)
